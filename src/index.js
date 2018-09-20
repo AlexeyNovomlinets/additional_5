d9b8112c50a01a2714502eb3ac5863709eb07e91
Aliaksei Navamlinets <alex.novomlinets@gmail.com>
2018-09-20 18:45:37 +0300

    feat: implement feature according to the task#6

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var inPairs = str.split('');
+  var stack = [];
+  var openPairs = [];
+  var closePairs = [];
+  var openIndex, closeIndex;
+  var key = false;
+
+  if ((str.length % 2) != 0) return false;
+
+  for (var i = 0; i < bracketsConfig.length; i++) {
+    openPairs[i] = bracketsConfig[i][0];
+    closePairs[i] = bracketsConfig[i][1];
+  }
+
+  for (i = 0; i < inPairs.length; i++) {
+    openIndex = openPairs.indexOf(inPairs[i]);
+    if (openIndex != -1 ) {
+      if ((closePairs.indexOf(inPairs[i]) != -1) && !key && stack.indexOf(openIndex) != -1) key = true;
+      if (!key) {
+        stack.push(openIndex);
+        continue;
+      }
+    }
+    closeIndex = closePairs.indexOf(inPairs[i]);
+    openIndex = stack.pop();
+    if (closeIndex != openIndex) return false;
+    key = false;
+  }
+
+  return true;
 }
